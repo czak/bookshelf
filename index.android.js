9334aa20f652be645213cae49c6730f1cb7b9c9b
Łukasz Adamczak <lukasz@czak.pl>
2016-09-22 23:36:31 +0200

    Simple layout with ToolbarAndroid & DrawerLayoutAndroid

--- index.android.js
+++ index.android.js
@@ -1,52 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ToolbarAndroid,
+  DrawerLayoutAndroid,
+  ListView,
 } from 'react-native';
 
 class bookshelf extends Component {
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        'Assassin Of Dusk',
+        'Lord Without Direction',
+        'Pilots Of Fire',
+        'Phantoms Without Hate',
+        'Gangsters And Serpents',
+        'Foreigners And Humans',
+        'Harmony Of The Ocean',
+        'Star Of Hell',
+        'Driving Into The Beginning',
+        'Faith Of The Dungeons',
+        'Lion Of Wood',
+        'Doctor Without Fear',
+        'Aliens Of The Solstice',
+        'Officers Of Gold',
+        'Lords And Foreigners',
+        'Girls And Traitors',
+        'Ruination Of The Gods',
+        'Termination Without Direction',
+        'Enter Nature',
+        'Faith Of The Jungle',
+      ])
+    };
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+    const navigationView = (
+      <View style={styles.drawer}>
+        <Text style={styles.drawerText}>I'm in the Drawer!</Text>
       </View>
+    );
+
+    return (
+      <DrawerLayoutAndroid
+        drawerWidth={300}
+        drawerPosition={DrawerLayoutAndroid.positions.Left}
+        renderNavigationView={() => navigationView}
+        ref={c => this._drawer = c}
+      >
+        <ToolbarAndroid
+          title="Bookshelf"
+          navIcon={require('./navigation_menu.png')}
+          onIconClicked={() => this._drawer.openDrawer()}
+          style={styles.toolbar}
+        />
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text style={styles.listItem}>{rowData}</Text>}
+        />
+      </DrawerLayoutAndroid>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  drawer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#444',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
+  drawerText: {
     margin: 10,
+    fontSize: 15,
+    textAlign: 'left',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  toolbar: {
+    height: 56,
+    backgroundColor: '#666',
+  },
+  listItem: {
+    padding: 10,
   },
 });
 
